c876564949d6e3b36c15cc4074218f5150a6f492
OengGechty <gechtyoeng@gmail.com>
2025-08-12 22:20:52 +0700

    config with tailwind and remove yarn

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css';
+
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4 rounded">
+      Tailwind is working!
+    </div>
+    
+  );
+}
+
+export default App;
