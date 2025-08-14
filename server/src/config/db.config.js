c8b259632c154e8b8bf6f5d0ec40a2e42d50afd6
veysean <veyseankem@gmail.com>
2025-08-14 14:09:29 +0700

    Add word translation

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -1,10 +1,10 @@
 import dotenv from 'dotenv';
-dotenv.config({path: '../.env'});
+dotenv.config();
 
 export default {
     HOST: process.env.DB_HOST,
     USER: process.env.DB_USER,
-    PASSWORD: process.env.DB_PASSWORD,
+    PASSWORD: process.env.DB_PASSWORD || '',
     DB: process.env.DB_NAME,
     PORT: process.env.DB_PORT,
     dialect: "mysql"
--- server/src/server.js
+++ server/src/server.js
@@ -4,13 +4,13 @@ console.log("Attempting to run server...");
 
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocs from './swagger.js'; 
+import swaggerDocs from './config/swagger.js'; 
 import db from './models/index.js'; 
 import authRoutes from './routes/authRoutes.js'; 
 import userRoutes from './routes/userRoutes.js';
 import wordTranslationRoutes from './routes/wordTranslationRoutes.js'; 
-import passport from './config/passport.config.js';
-import session from 'express-session';
+// import passport from './config/passport.config.js';
+// import session from 'express-session';
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -18,16 +18,16 @@ const PORT = process.env.PORT || 3002;
 app.use(express.json());
 
 // Correct order: Passport must be configured before the routes
-app.use(session({ secret: 'some_secret_key', resave: false, saveUninitialized: false, 
-cookie: {
-    maxAge: null, // session cookie
-  },
- }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(session({ secret: 'some_secret_key', resave: false, saveUninitialized: false, 
+// cookie: {
+//     maxAge: null, // session cookie
+//   },
+//  }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-app.use('/api/auth', authRoutes);
-app.use('/api', userRoutes); 
+// app.use('/api/auth', authRoutes);
+// app.use('/api', userRoutes); 
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/api', wordTranslationRoutes);
