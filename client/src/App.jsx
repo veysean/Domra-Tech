2ec3a9593f7cc8294acdea7b46c981e08a9f82bc
Chanalizain <inchanaliza@gmail.com>
2025-08-17 10:16:24 +0700

    completed the third party auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import './index.css';
 import { useTranslation } from 'react-i18next';
+import GoogleLoginButton from './component/googleLoginButton';
 
 function App() {
     const { t, i18n } = useTranslation();
@@ -10,10 +11,8 @@ function App() {
   return (
     <div>
       <h1>{t('welcome')}</h1>
-      <h1>{t('login')}</h1>
-      <h1>{t('logout')}</h1>
-      <button onClick={() => changeLanguage('en')}>English</button>
-      <button className='bg-amber-300 p-1.5' onClick={() => changeLanguage('kh')}>Khmer</button>
+      <GoogleLoginButton />
+
     </div>
     
   );
