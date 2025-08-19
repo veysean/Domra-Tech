0e485fd1ac4d5224f25b17ab85b98964605e152c
chanlinna <chanlinnalin@gmail.com>
2025-08-19 15:02:56 +0700

    home page

--- client/src/pages/user/Home.jsx
+++ client/src/pages/user/Home.jsx
@@ -1,8 +1,9 @@
+import HomeHeroSection from "../../components/HomeHeroSection";
+
 export default function Home() {
     return (
         <div className="home-page">
-            <h1>Welcome to the Home Page</h1>
-            {/* Additional content can be added here */}
+            <HomeHeroSection />
         </div>
     );
 }
