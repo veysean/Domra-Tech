cb328bc29198f1a8de1771775943d51dbdc49713
chanlinna <chanlinnalin@gmail.com>
2025-08-19 14:21:01 +0700

    update navbar

---
+++ client/src/pages/user/Categories.jsx
@@ -0,0 +1,8 @@
+export default function Categories() {
+    return (
+        <div className="categories-page">
+            <h1>Categories</h1>
+            {/* Additional content for categories can be added here */}
+        </div>
+    );
+}
