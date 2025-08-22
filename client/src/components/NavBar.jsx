72729409c0dd8ddc53ab4dbc10d8a8b8a511abcb
OengGechty <gechtyoeng@gmail.com>
2025-08-22 14:11:10 +0700

    add change language feature

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import i18n from "../i18n";
 export default function NavBar() {
 
-    const [open, setOPen] = useState(false);
-    const [language, setLanguage] = useState("ENG");
+    const [open, setOpen] = useState(false);
+    const [language, setLanguage] = useState(i18n.language === "kh" ? "KH" : "ENG");
     const [activeMenu, setActiveMenu] = useState("Home");
 
-    const handleSelect = (lang) => {
-        setLanguage(lang);
-        setOPen(false);
-    };
+    const languageMap = {
+        ENG: "en",
+        KH: "kh",
+        };
+
+   const handleSelect = (lang) => {
+            setLanguage(lang);       // update button text
+            i18n.changeLanguage(languageMap[lang]);
+            setOpen(false);
+        };
 
     const navItems = [
         { label: "Home", path: "/", width: "w-[64px]" },
@@ -58,14 +64,14 @@ export default function NavBar() {
 
                         {/* Button */}
                         <button 
-                            onClick={() => setOPen(!open)}
-                            className="w-[113px] h-[44px] bg-[#F8F9FA] rounded-[30px] outline outline-[1px] outline-[#E2E8F0] outline-offset-[-1px] flex justify-center items-center"
+                            onClick={() => setOpen(!open)}
+                            className="w-[113px] h-[44px] bg-[#F8F9FA] rounded-[30px] outline-[1px] outline-[#E2E8F0] outline-offset-[-1px] flex justify-center items-center"
                         >
                             <div className="flex justify-center items-center">
                                 <div className="w-[55px] text-[#667EEA]/80 text-[20px] font-semibold font-inter"> {language} </div>
                                 <div className="w-[19px]">
                                     <svg width="20" height="10" viewBox="0 0 20 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M0.714666 1.04808C0.78254 0.993216 0.863172 0.949687 0.951943 0.919987C1.04071 0.890287 1.13588 0.875 1.23199 0.875C1.3281 0.875 1.42327 0.890287 1.51204 0.919987C1.60081 0.949687 1.68144 0.993216 1.74931 1.04808L10.0002 7.70177L18.2511 1.04808C18.319 0.993305 18.3997 0.949855 18.4884 0.92021C18.5772 0.890566 18.6723 0.875308 18.7684 0.875308C18.8645 0.875308 18.9596 0.890566 19.0484 0.92021C19.1371 0.949855 19.2178 0.993305 19.2857 1.04808C19.3537 1.10286 19.4075 1.16788 19.4443 1.23945C19.4811 1.31102 19.5 1.38772 19.5 1.46519C19.5 1.54265 19.4811 1.61936 19.4443 1.69093C19.4075 1.76249 19.3537 1.82752 19.2857 1.8823L10.5175 8.95192C10.4496 9.00678 10.369 9.05031 10.2802 9.08001C10.1915 9.10971 10.0963 9.125 10.0002 9.125C9.90408 9.125 9.80892 9.10971 9.72015 9.08001C9.63137 9.05031 9.55074 9.00678 9.48287 8.95192L0.714666 1.8823C0.64662 1.82757 0.592633 1.76256 0.555797 1.69098C0.518961 1.61941 0.5 1.54268 0.5 1.46519C0.5 1.3877 0.518961 1.31097 0.555797 1.23939C0.592633 1.16782 0.64662 1.10281 0.714666 1.04808Z" fill="url(#paint0_linear_528_682)" fill-opacity="0.8"/>
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M0.714666 1.04808C0.78254 0.993216 0.863172 0.949687 0.951943 0.919987C1.04071 0.890287 1.13588 0.875 1.23199 0.875C1.3281 0.875 1.42327 0.890287 1.51204 0.919987C1.60081 0.949687 1.68144 0.993216 1.74931 1.04808L10.0002 7.70177L18.2511 1.04808C18.319 0.993305 18.3997 0.949855 18.4884 0.92021C18.5772 0.890566 18.6723 0.875308 18.7684 0.875308C18.8645 0.875308 18.9596 0.890566 19.0484 0.92021C19.1371 0.949855 19.2178 0.993305 19.2857 1.04808C19.3537 1.10286 19.4075 1.16788 19.4443 1.23945C19.4811 1.31102 19.5 1.38772 19.5 1.46519C19.5 1.54265 19.4811 1.61936 19.4443 1.69093C19.4075 1.76249 19.3537 1.82752 19.2857 1.8823L10.5175 8.95192C10.4496 9.00678 10.369 9.05031 10.2802 9.08001C10.1915 9.10971 10.0963 9.125 10.0002 9.125C9.90408 9.125 9.80892 9.10971 9.72015 9.08001C9.63137 9.05031 9.55074 9.00678 9.48287 8.95192L0.714666 1.8823C0.64662 1.82757 0.592633 1.76256 0.555797 1.69098C0.518961 1.61941 0.5 1.54268 0.5 1.46519C0.5 1.3877 0.518961 1.31097 0.555797 1.23939C0.592633 1.16782 0.64662 1.10281 0.714666 1.04808Z" fill="url(#paint0_linear_528_682)" fill-opacity="0.8"/>
                                     <defs>
                                     <linearGradient id="paint0_linear_528_682" x1="3.5809" y1="4.12183" x2="17.8791" y2="3.7978" gradientUnits="userSpaceOnUse">
                                     <stop stop-color="#667EEA"/>
