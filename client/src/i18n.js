7a39774f7a73721417d1e459cda5adb73ee819ec
SSONita <sarsovannita4@gmail.com>
2025-08-25 04:46:29 +0700

    switch language for sidebar

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -2,13 +2,22 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './locales/en/common.json';
 import kh from './locales/kh/common.json';
+import enSidebar from './locales/en/adSidebar.json';
+import khSidebar from './locales/kh/adSidebar.json';
 
 i18n
   .use(initReactI18next)
   .init({
     resources: {
-      en: { translation: en },
-      kh: { translation: kh },
+      en: { 
+        translation: en,
+        adSidebar: enSidebar
+
+       },
+      kh: { 
+        translation: kh,
+        adSidebar: khSidebar
+      },
     },
     lng: 'en', // default language
     fallbackLng: 'en',
