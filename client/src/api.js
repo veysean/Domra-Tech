e418efb94832aef28dcbaba8da2ae510a69c629d
chanlinna <chanlinnalin@gmail.com>
2025-08-28 14:10:55 +0700

    Navbar

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:4000/api', //backend URL
+    baseURL: 'http://localhost:3000/api', //backend URL
 });
 
 const token = localStorage.getItem('token');
