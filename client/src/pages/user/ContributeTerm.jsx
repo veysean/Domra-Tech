91fbcfa9dea5813f1c565e3d5b062c99f60429b1
OengGechty <gechtyoeng@gmail.com>
2025-08-28 11:19:28 +0700

    add request changing guidline

--- client/src/pages/user/ContributeTerm.jsx
+++ client/src/pages/user/ContributeTerm.jsx
@@ -6,8 +6,10 @@ import { useTranslation } from 'react-i18next';
 import { motion } from "framer-motion";
 import requestImg from "../../assets/request.png";
 import contributeImg from "../../assets/contribute.png";
+import API from "../../api";
 import RequestChangingForm from "../../components/user/RequestChangingForm";
 import RequestNewWordForm from "../../components/user/RequestNewWordForm";
+import GuidLine from "../../components/user/GuidLine";
 export default function ContributeTerm() {
     const { t } = useTranslation('contributeTerm');
     const [isRequestNewWord, setIsRequestNewWord] = useState(false);
@@ -39,13 +41,15 @@ export default function ContributeTerm() {
             animate={{ opacity: 1, y: 0, transition: { duration: 0.4, delay: 0.1} }}
             exit={{ opacity: 0, y: -20, transition: { duration: 0.4, delay: 0.1} }}
         >
-            <div className="contribute-term-page">
+            <div className="contribute-term-page pb-10">
                 <HomeHeroSection header={t("contributeHeader")} desc={t("contributeDesc")} />
-
+                <div className="flex justify-center m-10">
+                    <GuidLine />
+                </div>
                 {isRequestNewWord || isRequestChanging ? null : (
-                <div className="flex justify-center gap-20 m-10">
+                <div className="w-[1156px] m-auto flex justify-between">
                 {/* add new word request */}
-                <div className="w-[467px] p-7 bg-white rounded-[30px] outline-1 outline-offset-[-1px] outline-slate-200 inline-flex flex-col justify-center items-center gap-5 hover:shadow-lg transition-shadow duration-300">
+                <div className="group w-[467px] p-7 bg-white rounded-[30px] outline-1 outline-offset-[-1px] outline-slate-200 inline-flex flex-col justify-center items-center gap-5 hover:shadow-lg transition-shadow duration-300">
                     <div className="self-stretch flex flex-col justify-start items-center gap-12">
                         <div className="self-stretch h-10 text-center justify-start text-indigo-500 text-3xl font-medium font-['Inter'] group-hover:font-bold">Add New Terms</div>
                         <div className="self-stretch text-center justify-start text-slate-600 text-base font-normal font-['Inter']">Submit new technical terms with accurate Khmer translations</div>
@@ -56,9 +60,9 @@ export default function ContributeTerm() {
                         <button className="px-5 py-3.5 bg-indigo-500 rounded-[30px] text-white text-xl font-bold font-['Inter'] hover:bg-indigo-600"
                         onClick={() => auth ? setIsRequestNewWord(true) : navigate("/auth")}
                         >Submit Term</button>
-                    </div>
+                </div>
                 {/* request changing */}
-                <div className="w-[467px] p-7 bg-white rounded-[30px] outline-1 outline-offset-[-1px] outline-slate-200 inline-flex flex-col justify-center items-center gap-5 hover:shadow-lg transition-shadow duration-300">
+                <div className="group w-[467px] p-7 bg-white rounded-[30px] outline-1 outline-offset-[-1px] outline-slate-200 inline-flex flex-col justify-center items-center gap-5 hover:shadow-lg transition-shadow duration-300">
                     <div className="self-stretch flex flex-col justify-start items-center gap-12">
                         <div className="self-stretch h-10 text-center justify-start text-indigo-500 text-3xl font-medium font-['Inter'] group-hover:font-bold"
                         >Improve Translations</div>
