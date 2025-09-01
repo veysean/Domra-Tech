bc2d77fd397bc9cba484b73b30222b42c86a9af5
chanlinna <chanlinnalin@gmail.com>
2025-09-01 09:12:47 +0700

    add style

--- client/src/components/user/CategorySearch.jsx
+++ client/src/components/user/CategorySearch.jsx
@@ -20,7 +20,7 @@ export default function CategorySearch({ onSearchChange, onSearchSubmit, onSearc
     };
     return (
         <div className="flex justify-center w-full">
-            <div className="w-[1156px] p-10 rounded-[20px] shadow-[1px_3px_0px_0px_rgba(0,0,0,0.05)] outline-1 outline-offset-[-1px] outline-slate-200 inline-flex flex-col justify-start items-start gap-7">
+            <div className="w-[1156px] p-10 rounded-[20px] shadow-[1px_3px_0px_0px_rgba(0,0,0,0.05)] hover:shadow-[0px_8px_8px_0px_rgba(0,0,0,0.25)] outline-1 outline-offset-[-1px] outline-slate-200 inline-flex flex-col justify-start items-start gap-7">
                 {/* Search bar */}
                 <SearchBar onSearchChange={handleSearch} onSearchSubmit={handleSearchSubmit}  onSearchFocus={handleSearchFocus}/>
 
