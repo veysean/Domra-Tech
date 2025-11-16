cc14838023d876f76ff6c37f37ec71c61ed3e2ea
Chanalizain <inchanaliza@gmail.com>
2025-11-17 00:04:57 +0700

    implement share api

--- server/src/routes/wordTranslationRoutes.js
+++ server/src/routes/wordTranslationRoutes.js
@@ -3,11 +3,16 @@ import wordTranslationController from '../controllers/wordTranslationController.
 import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
-
+const { verifyAuth } = authMiddleware;
 
 router.get('/words/search', wordTranslationController.searchWords);
 router.get('/words', wordTranslationController.findAll);
 router.get('/words/:wordId', wordTranslationController.findById);
+
+router.get('/wordcards/:wordId', wordTranslationController.getPublicWordCard);
+router.get('/share/:wordId', wordTranslationController.renderSharedCardPage);
+router.get('/words/:wordId/share', wordTranslationController.findById);
+
 router.post('/admin/words', authMiddleware.verifyAuth, authMiddleware.checkAdminRole, wordTranslationController.create);
 router.put('/admin/words/:wordId', authMiddleware.verifyAuth, authMiddleware.checkAdminRole, wordTranslationController.update);
 router.delete('/admin/words/:wordId', authMiddleware.verifyAuth, authMiddleware.checkAdminRole, wordTranslationController.remove);
