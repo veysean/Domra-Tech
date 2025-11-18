b7d9026611665964bbffc901683c0f6b84bee039
vesyean <veyseankem@gmail.com>
2025-11-18 10:12:38 +0700

    Completed feature sign in/up with third party

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,35 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App.jsx';
+// import './i18n';
+// import { AuthProvider } from "./contexts/AuthContext";
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <AuthProvider>
+//       <App />
+//     </AuthProvider>
+//   </React.StrictMode>
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import './i18n';
 import { AuthProvider } from "./contexts/AuthContext";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <GoogleOAuthProvider clientId='2457929257-bpmmhnns2un9v6do63ks7ico2gqk16e5.apps.googleusercontent.com'>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
+// clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
