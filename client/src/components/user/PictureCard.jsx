8fbcbcbdd6d64fbd7ac271ccaeebfdc7e37ff5fb
chanlinna <chanlinnalin@gmail.com>
2025-11-20 20:12:51 +0700

    fix/update color and translation of about us page in index.css, PictureCard.jsx, aboutUs.json, AboutUs.jsx, package

--- client/src/components/user/PictureCard.jsx
+++ client/src/components/user/PictureCard.jsx
@@ -5,8 +5,8 @@ const PictureCard = ({ imageSrc, name, role }) => {
     <div data-property-1="Default" className=" p-5 bg-white rounded-[20px] outline-1 outline-offset-[-1px] outline-slate-200 inline-flex flex-col justify-center items-center gap-5 hover:shadow-lg transition-shadow duration-300">
     {/* <img className="self-stretch rounded-[20px]" src={imageSrc} alt={name} /> */}
     <div className="self-stretch h-20 flex flex-col justify-start items-center gap-2.5">
-        <div className="text-center text-indigo-500 lg:text-xl font-medium font-['Inter']">{name}</div>
-        <div className="text-center text-slate-600 lg:text-xl font-normal font-['Inter']">{role}</div>
+        <div className="text-center text-main-color lg:text-xl font-medium">{name}</div>
+        <div className="text-center text-slate-600 lg:text-xl font-normal">{role}</div>
     </div>
 </div>
   );
