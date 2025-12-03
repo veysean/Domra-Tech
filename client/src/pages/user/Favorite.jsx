f27bf2cbe78f0e5cf92c5a767aa6e71b00b6fde1
vesyean <veyseankem@gmail.com>
2025-12-03 09:40:10 +0700

    merge all fixing in client and admin

--- client/src/pages/user/Favorite.jsx
+++ client/src/pages/user/Favorite.jsx
@@ -1,9 +1,11 @@
 import { FavoriteServices } from "../../api";
 import { useEffect, useState } from "react";
 import WordCard from "../../components/user/WordCard";
+import { useTranslation } from 'react-i18next';
 
 export default function Favorite({ userData }) {
     const [favorites, setFavorites] = useState([]);
+    const { t } = useTranslation('profilePage');
 
     useEffect(() => {
         const fetchFavorites = async () => {
@@ -16,7 +18,7 @@ export default function Favorite({ userData }) {
 
     return(
         <div>
-            <div className="text-indigo-500 text-2xl font-bold hidden lg:block">Your Favorites</div>
+            <div className="text-indigo-500 text-2xl font-bold hidden lg:block">{t('yourfavourite')}</div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10 gap-4">
                 {favorites.map((word) => (
                     <WordCard word={word} />
